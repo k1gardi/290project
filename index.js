365291b90ab4581419ffef2c640cf21445dd34e7
k1gardi <55878920+k1gardi@users.noreply.github.com>
2021-03-02 21:03:14 -0600

    finished carousel and image mapping

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.set('view engine', 'hbs')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.static('views/images'))
+
 
 app.get('/', (req, res) => {
-	res.render('home')
+	res.render('home', req.static)
 
 });
 
